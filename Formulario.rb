d4f175be4f5c5a6b2be63d0fe1371b02992bf530
OihanGonzalez <gonzalezoihan8@gmail.com>
2026-05-05 13:58:00 +0200

    Instartar en formulario funcional

--- Formulario.rb
+++ Formulario.rb
@@ -1,80 +1,84 @@
 require 'glimmer-dsl-libui'
-
-include Glimmer
-
-def ventana()
-  window('Form') {
-    margined true
-    
-    vertical_box {
-      form {
-        @id = entry {
-          label 'ID' # label property is available when control is nested under form
-        }
-        
-        @user = entry {
-          label 'User' # label property is available when control is nested under form
-        }
-        
-        @password = entry {
-          label 'Password' # label property is available when control is nested under form
-        }
-        
-
-      }
-
-      button('Insert') {
-        stretchy false
-        on_clicked do
-        if @id.text != "" && @user.text != "" && @password.text != "" 
-
-          @id.text = "";
-          @user.text = "";
-          @password.text = "";
-        else
-          msg_box('Error', "No puede haber campos vacios")
-        end
-      end
-      }
-
-      button('Delete') {
-        stretchy false
-        on_clicked do
-        
-        @user.text = "";
-          
-      end
-      }
-
-      button('Update') {
-        stretchy false
-        on_clicked do
-        if @user.text != ""
-
-          @user.text = "";
-        else
-          msg_box('Error', "Rellena el nombre")
-        end
-      end
-      }
-    }
-  }.show
-end
-
-ventana()
-
-
 require_relative 'ddbb'
 
 class Formulario
-  def otro
-    db = Implementacion.new
-    if db.insert()
-      puts("Entra")
-    else
-      msg_box('Error', "Error")
-    end
+
+include Glimmer
+
+  def ventana()
+    window('Form') {
+      margined true
+      
+      vertical_box {
+        form {
+          @id = entry {
+            label 'ID' # label property is available when control is nested under form
+          }
+          
+          @user = entry {
+            label 'User' # label property is available when control is nested under form
+          }
+          
+          @password = entry {
+            label 'Password' # label property is available when control is nested under form
+          }
+          @tipo = radio_buttons {
+            items 'Cliente', 'Admin'
+            selected 0
+          }
+
+        }
+
+        button('Insert') {
+          stretchy false
+          on_clicked do
+          if @id.text != "" && @user.text != "" && @password.text != "" 
+            tipoUsuario = case @tipo.selected
+                          when 0 then 'Cliente'
+                          when 1 then 'Admin'
+                          end
+            msg_box('Info', "El usuario #{@user.text} con id #{@id.text} y contraseña #{@password.text} es #{tipoUsuario}")
+            self.insertar(@id.text, @user.text, @password.text, tipoUsuario)
+            @id.text = "";
+            @user.text = "";
+            @password.text = "";
+          else
+            msg_box('Error', "No puede haber campos vacios")
+          end
+        end
+        }
+
+        button('Delete') {
+          stretchy false
+          on_clicked do
+          @user.text = "";
+        end
+        }
+
+        button('Update') {
+          stretchy false
+          on_clicked do
+          if @user.text != ""
+
+            @user.text = "";
+          else
+            msg_box('Error', "Rellena el nombre")
+          end
+        end
+        }
+      }
+    }.show
   end
+
+  def insertar(id, user, password, tipoUsuario)
+      puts("Entra")
+      db = Implementacion.new
+      if db.insert(id, user, password, tipoUsuario)
+        puts("Entra")
+      else
+        msg_box('Error', "Error")
+      end
+    end
 end
 
-Formulario.new.otro
+Formulario.new.ventana
