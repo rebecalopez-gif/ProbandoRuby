ee038a86510e47a8fe0a0ade0602cf1c967ffacf
rebecalopez-gif <rebeca.curso2526@gmail.com>
2026-05-07 09:09:59 +0200

    cambios

--- Ficheros.rb
+++ Ficheros.rb
@@ -1,27 +1,23 @@
 if File.exist?("prueba.txt")
-  File.open("prueba.txt","r+") do |f|  #w para modo escritura
+  File.open("prueba.txt","r+") do |f|  #r+ para modo lectura y escritura
     contenido_anterior = f.read
     puts contenido_anterior
     f.write("\nNueva linea")
   end
 end
 
+File.open("prueba2.txt","w") do |archivo|  #w para modo escritura
+  archivo.puts "Texto"
+end
 
-
-
-
-
-
-=begin
-
-if File.exist?("prueba.txt")
-  File.open("prueba.txt","r") do |archivo|  #r para modo lectura
+if File.exist?("prueba2.txt")
+  File.open("prueba2.txt","r") do |archivo|  #r para modo lectura
     archivo.each_line do |linea|
       puts linea
     end
   end
 end
-=end
+
 
 
 
